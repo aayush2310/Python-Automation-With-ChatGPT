3d60e5a13517f23107d21f84433d5b9147c6b6b8
aayush2310 <99485539+aayush2310@users.noreply.github.com>
2023-06-25 16:23:15 +0530

    Add files via upload

---
+++ Automation-Script-for-cleaning-Downloads-Folder/1 python-chatgpt.py
@@ -0,0 +1,36 @@
+import requests
+import argparse
+import os
+
+parser = argparse.ArgumentParser() 
+parser.add_argument("prompt",help="The prompt to send to the OpenAI API")  
+parser.add_argument("file_name",help="Name of the file to save Python Script")  
+
+args = parser.parse_args()
+
+api_endpoint = "https://api.openai.com/v1/completions"
+api_key = os.getenv("OPENAI_API_KEY")
+
+request_headers = {
+    "Content-Type": "application/json",
+    "Authorization" : "Bearer" + api_key
+}
+
+
+request_data = {
+    "model" : "text-davinci-003",
+    "prompt" : f"Write python script to {args.prompt}. Provide only code, no text",  
+    "max_tokens": 1000,
+    "temperature" : 0.5
+}
+
+response = requests.post(api_endpoint, headers=request_headers, json=request_data)
+
+if response.status_code == 200:
+              response_text = response.json()["choices"][0]["text"]
+              with open("args.file_name", "w") as file:
+                      file.write(response_text)
+else:
+               print(f"Request failed with status code: {str(response.status_code)}")
+
+#i will run this code in terminal and it will generate a python script for my task in "clean-downloads.py" file.
